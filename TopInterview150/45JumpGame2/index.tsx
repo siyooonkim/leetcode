faecc48cce9a272da441fe725e141a356a257b18
siyooonkim <ksyoon0718@gmail.com>
2024-10-15 16:47:00 +0900

    [45. Jump game2]

--- TopInterview150/45JumpGame2/index.tsx
+++ TopInterview150/45JumpGame2/index.tsx
@@ -1,1 +1,19 @@
-function jump(nums: number[]): number {}
+function jump(nums: number[]): number {
+  let jump = 0;
+  let curMax = 0;
+  let nextMax = 0;
+  let end = nums.length - 1;
+
+  for (let i = 0; i < end; i++) {
+    if (i + nums[i] > nextMax) {
+      nextMax = i + nums[i];
+    }
+
+    if (i == curMax) {
+      jump += 1;
+      curMax = nextMax;
+      if (curMax >= end) break;
+    }
+  }
+  return jump;
+}
