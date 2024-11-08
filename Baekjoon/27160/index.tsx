f6a2ddc3b4fdbaaf4ca2cf2bbcb9c65c864010e0
siyooonkim <ksyoon0718@gmail.com>
2024-11-08 20:10:51 +0900

    [Baekjoon] 7562

--- Baekjoon/27160/index.tsx
+++ Baekjoon/27160/index.tsx
@@ -1,31 +1,31 @@
-function processInput(): void {
-  const fs = require('fs');
-  const input = fs.readFileSync('input.txt').toString().trim().split('\n');
+// function processInput(): void {
+//   const fs = require('fs');
+//   const input = fs.readFileSync('input.txt').toString().trim().split('\n');
 
-  // 첫 번째 줄의 숫자(n)
-  const n = parseInt(input[0]);
+//   // 첫 번째 줄의 숫자(n)
+//   const n = parseInt(input[0]);
 
-  // 각 줄을 배열로 저장
-  let inputArr = [];
+//   // 각 줄을 배열로 저장
+//   let inputArr = [];
 
-  for (let i = 1; i <= n; i++) {
-    const [fruit, count] = input[i].split(' '); // 각 줄에서 과일 이름과 개수를 분리
-    inputArr.push({ fruit, count: parseInt(count) });
-  }
+//   for (let i = 1; i <= n; i++) {
+//     const [fruit, count] = input[i].split(' '); // 각 줄에서 과일 이름과 개수를 분리
+//     inputArr.push({ fruit, count: parseInt(count) });
+//   }
 
-  let hash = new Map(); // {fruit: count}
+//   let hash = new Map(); // {fruit: count}
 
-  for (let i = 0; i < inputArr.length; i++) {
-    let fruitCount = hash.get(inputArr[i]['fruit']);
-    hash.set(inputArr[i]['fruit'], fruitCount ? fruitCount + inputArr[i]['count'] : inputArr[i]['count']);
-  }
+//   for (let i = 0; i < inputArr.length; i++) {
+//     let fruitCount = hash.get(inputArr[i]['fruit']);
+//     hash.set(inputArr[i]['fruit'], fruitCount ? fruitCount + inputArr[i]['count'] : inputArr[i]['count']);
+//   }
 
-  for (let [key, value] of hash) {
-    if (value == 5) console.log(true);
-    return;
-  }
-  console.log(false);
-  return;
-}
+//   for (let [key, value] of hash) {
+//     if (value == 5) console.log(true);
+//     return;
+//   }
+//   console.log(false);
+//   return;
+// }
 
-processInput();
+// processInput();
--- Baekjoon/31562/index.tsx
+++ Baekjoon/31562/index.tsx
@@ -1,40 +1,40 @@
-const fs = require('fs');
-const input = fs.readFileSync('input.txt').toString().trim().split('\n');
+// const fs = require('fs');
+// const input = fs.readFileSync('input.txt').toString().trim().split('\n');
 
-// 첫 번째 줄의 N과 M을 읽어옵니다.
-const [N, M] = input[0].split(' ').map(Number);
+// // 첫 번째 줄의 N과 M을 읽어옵니다.
+// const [N, M] = input[0].split(' ').map(Number);
 
-// N개의 노래 정보를 저장할 객체를 생성합니다.
-// const knownSongs: { [key: string]: string } = {};
-const knownSongs = new Map();
+// // N개의 노래 정보를 저장할 객체를 생성합니다.
+// // const knownSongs: { [key: string]: string } = {};
+// const knownSongs = new Map();
 
-// 2번째 줄부터 N개의 줄까지는 노래 제목, 길이, 음 정보를 포함하고 있습니다.
-for (let i = 1; i <= N; i++) {
-  const [T, S, ...notes] = input[i].split(' ');
-  const notePattern = notes.slice(0, 3).join('');
+// // 2번째 줄부터 N개의 줄까지는 노래 제목, 길이, 음 정보를 포함하고 있습니다.
+// for (let i = 1; i <= N; i++) {
+//   const [T, S, ...notes] = input[i].split(' ');
+//   const notePattern = notes.slice(0, 3).join('');
 
-  if (!knownSongs.has(notePattern)) {
-    knownSongs.set(notePattern, []);
-  }
+//   if (!knownSongs.has(notePattern)) {
+//     knownSongs.set(notePattern, []);
+//   }
 
-  knownSongs.get(notePattern).push(S);
-}
+//   knownSongs.get(notePattern).push(S);
+// }
 
-// N+2번째 줄부터 M개의 줄까지는 정환이 맞히기를 시도할 노래의 음 정보를 저장합니다.
-const attempts = [];
-for (let i = N + 1; i < N + 1 + M; i++) {
-  const info = input[i].split(' ').join('');
-  attempts.push(info);
-}
+// // N+2번째 줄부터 M개의 줄까지는 정환이 맞히기를 시도할 노래의 음 정보를 저장합니다.
+// const attempts = [];
+// for (let i = N + 1; i < N + 1 + M; i++) {
+//   const info = input[i].split(' ').join('');
+//   attempts.push(info);
+// }
 
-for (const attempt of attempts) {
-  const title = knownSongs.get(attempt);
-  // console.log(title);
-  if (!title) {
-    console.log('!');
-  } else if (title.length >= 2) {
-    console.log('?');
-  } else {
-    console.log(title[0]);
-  }
-}
+// for (const attempt of attempts) {
+//   const title = knownSongs.get(attempt);
+//   // console.log(title);
+//   if (!title) {
+//     console.log('!');
+//   } else if (title.length >= 2) {
+//     console.log('?');
+//   } else {
+//     console.log(title[0]);
+//   }
+// }
