16a34c4debe240254c9aa1f505ffe0169c9808de
siyooonkim <ksyoon0718@gmail.com>
2024-11-14 18:33:41 +0900

    [Baekjoon] 318926

--- Baekjoon/2847/index.tsx
+++ Baekjoon/2847/index.tsx
@@ -1,17 +1,17 @@
-let fs = require('fs');
-// const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
-const input = fs.readFileSync('input.txt').toString().trim().split('\n');
+// let fs = require('fs');
+// // const input = fs.readFileSync('/dev/stdin').toString().trim().split(' ');
+// const input = fs.readFileSync('input.txt').toString().trim().split('\n');
 
-const N = Number(input[0]);
-const scores = input.slice(1).map(Number);
+// const N = Number(input[0]);
+// const scores =
 
-let count = 0;
+// let count = 0;
 
-for (let i = N - 2; i >= 0; i--) {
-  if (scores[i] >= scores[i + 1]) {
-    count += scores[i] - (scores[i + 1] - 1);
-    scores[i] = scores[i + 1] - 1;
-  }
-}
+// for (let i = N - 2; i >= 0; i--) {
+//   if (scores[i] >= scores[i + 1]) {
+//     count += scores[i] - (scores[i + 1] - 1);
+//     scores[i] = scores[i + 1] - 1;
+//   }
+// }
 
-console.log(count);
+// console.log(count);
