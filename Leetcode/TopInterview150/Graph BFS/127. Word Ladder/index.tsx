c2e8feef041158ea2b92cd9c9b0d0db131605c10
siyooonkim <ksyoon0718@gmail.com>
2025-07-16 12:03:39 +0900

    141. Linked List Cycle

--- Leetcode/TopInterview150/Graph BFS/127. Word Ladder/index.tsx
+++ Leetcode/TopInterview150/Graph BFS/127. Word Ladder/index.tsx
@@ -1,45 +1,49 @@
+// 수정 버전 다시 풀어보기
 function ladderLength(beginWord: string, endWord: string, wordList: string[]): number {
-  const queue: [string, number][] = [];
-  const visited = new Set<string>();
+  const letters = Array.from({ length: 26 }, (_, i) => String.fromCharCode(97 + i));
+  let wordSet = new Set(wordList);
+  let start = new Set([beginWord]);
+  let end = new Set([endWord]);
+  let counts = 1;
 
-  if (wordList.includes(endWord) === false) return 0;
+  if (!wordSet.has(endWord)) return 0;
 
-  function addQueue(word: string, counts: number): void {
-    for (let i = 0; i < wordList.length; i++) {
-      if (visited.has(wordList[i])) continue;
-      let diff = 0;
-      for (let j = 0; j < word.length; j++) {
-        if (wordList[i][j] !== word[j]) {
-          diff++;
+  while (start.size) {
+    const nextStart = new Set<string>([]);
+    for (let word of start) {
+      let wordArr = word.split('');
+      for (let i = 0; i < wordArr.length; i++) {
+        const oldChar = wordArr[i];
+        for (let j = 0; j < letters.length; j++) {
+          if (oldChar === letters[j]) continue;
+
+          wordArr[i] = letters[j];
+          const newWord = wordArr.join('');
+          if (end.has(newWord)) return counts + 1;
+          if (wordSet.has(newWord)) {
+            nextStart.add(newWord);
+            wordSet.delete(newWord);
+          }
         }
-      }
-      if (diff === 1) {
-        queue.push([wordList[i], counts + 1]);
-        visited.add(wordList[i]);
+        wordArr[i] = oldChar;
       }
     }
+
+    start = nextStart;
+    counts++;
   }
-
-  addQueue(beginWord, 1);
-
-  while (queue.length > 0) {
-    let [word, counts] = queue.shift()!;
-    if (word === endWord) return counts;
-    addQueue(word, counts);
-  }
-
   return 0;
 }
 
 // function ladderLength(beginWord: string, endWord: string, wordList: string[]): number {
 //   const queue: [string, number][] = [];
-//   const visited = new Array(wordList.length).fill(false);
+//   const visited = new Set<string>();
 
 //   if (wordList.includes(endWord) === false) return 0;
 
 //   function addQueue(word: string, counts: number): void {
 //     for (let i = 0; i < wordList.length; i++) {
-//       if (visited[i]) continue;
+//       if (visited.has(wordList[i])) continue;
 //       let diff = 0;
 //       for (let j = 0; j < word.length; j++) {
 //         if (wordList[i][j] !== word[j]) {
@@ -48,7 +52,7 @@ function ladderLength(beginWord: string, endWord: string, wordList: string[]): n
 //       }
 //       if (diff === 1) {
 //         queue.push([wordList[i], counts + 1]);
-//         visited[i] = true;
+//         visited.add(wordList[i]);
 //       }
 //     }
 //   }
@@ -56,10 +60,43 @@ function ladderLength(beginWord: string, endWord: string, wordList: string[]): n
 //   addQueue(beginWord, 1);
 
 //   while (queue.length > 0) {
-//     let [word, counts] = queue.shift();
+//     let [word, counts] = queue.shift()!;
 //     if (word === endWord) return counts;
 //     addQueue(word, counts);
 //   }
 
 //   return 0;
 // }
+
+// // function ladderLength(beginWord: string, endWord: string, wordList: string[]): number {
+// //   const queue: [string, number][] = [];
+// //   const visited = new Array(wordList.length).fill(false);
+
+// //   if (wordList.includes(endWord) === false) return 0;
+
+// //   function addQueue(word: string, counts: number): void {
+// //     for (let i = 0; i < wordList.length; i++) {
+// //       if (visited[i]) continue;
+// //       let diff = 0;
+// //       for (let j = 0; j < word.length; j++) {
+// //         if (wordList[i][j] !== word[j]) {
+// //           diff++;
+// //         }
+// //       }
+// //       if (diff === 1) {
+// //         queue.push([wordList[i], counts + 1]);
+// //         visited[i] = true;
+// //       }
+// //     }
+// //   }
+
+// //   addQueue(beginWord, 1);
+
+// //   while (queue.length > 0) {
+// //     let [word, counts] = queue.shift();
+// //     if (word === endWord) return counts;
+// //     addQueue(word, counts);
+// //   }
+
+// //   return 0;
+// // }
---
+++ Leetcode/TopInterview150/LinkedList/141. Linked List Cycle/index.tsx
@@ -0,0 +1,29 @@
+// Hash Table 방법
+// 시간 복잡도 O(n)
+// 공간 복잡도 O(n) Set을 사용했는데 최대 O(n) 까지 갈 수 있음
+// /**
+//  * Definition for singly-linked list.
+//  * class ListNode {
+//  *     val: number
+//  *     next: ListNode | null
+//  *     constructor(val?: number, next?: ListNode | null) {
+//  *         this.val = (val===undefined ? 0 : val)
+//  *         this.next = (next===undefined ? null : next)
+//  *     }
+//  * }
+//  */
+
+// function hasCycle(head: ListNode | null): boolean {
+//   if (head) {
+//     let nextNode: ListNode | null = head.next;
+//     let visited = new Set<ListNode>();
+//     visited.add(head);
+//     while (nextNode) {
+//       if (visited.has(nextNode)) return true;
+//       visited.add(nextNode);
+//       nextNode = nextNode.next;
+//     }
+//     return false;
+//   }
+//   return false;
+// }
