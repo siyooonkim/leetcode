097fa76a1dd1ba9dbc3c6e81ce421a069e53af47
siyooonkim <ksyoon0718@gmail.com>
2025-07-16 12:20:23 +0900

    141. Linked List Cycle

--- Leetcode/TopInterview150/LinkedList/141. Linked List Cycle/index.tsx
+++ Leetcode/TopInterview150/LinkedList/141. Linked List Cycle/index.tsx
@@ -1,3 +1,35 @@
+// Two pointer 방법
+// 시간 복잡도 O(n)
+// 공간 복잡도 O(1)
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function hasCycle(head: ListNode | null): boolean {
+  if (head === null || head.next === null) {
+    return false;
+  }
+
+  let slowPointer: ListNode | null = head;
+  let fastPointer: ListNode | null = head.next;
+
+  while (slowPointer !== fastPointer) {
+    if (fastPointer == null || fastPointer.next == null) return false;
+    slowPointer = slowPointer.next;
+    fastPointer = fastPointer.next.next;
+  }
+  return true;
+}
+
 // Hash Table 방법
 // 시간 복잡도 O(n)
 // 공간 복잡도 O(n) Set을 사용했는데 최대 O(n) 까지 갈 수 있음
