a7c2c2794c019aaa5271b06449aa5bca94482498
siyooonkim <ksyoon0718@gmail.com>
2025-07-17 11:46:53 +0900

    2. Add Two Numbers

---
+++ Leetcode/TopInterview150/LinkedList/2. Add Two Numbers/index.tsx
@@ -0,0 +1,47 @@
+// 첫 번째 풀이
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function addTwoNumbers(l1: ListNode | null, l2: ListNode | null): ListNode | null {
+  let currL1: ListNode | null = l1;
+  let currL2: ListNode | null = l2;
+  let firstNode = true;
+  let share = 0;
+  let nodeArr: number[] = [];
+
+  while (currL1 || currL2 || share) {
+    const l1Val = currL1 ? currL1.val : 0;
+    const l2Val = currL2 ? currL2.val : 0;
+
+    const sum = l1Val + l2Val + share;
+    const remain = sum % 10;
+    nodeArr.push(remain);
+    share = Math.floor(sum / 10);
+
+    currL1 = currL1 ? currL1.next : null;
+    currL2 = currL2 ? currL2.next : null;
+  }
+
+  let l3: ListNode | null = null;
+
+  for (let i = nodeArr.length - 1; i >= 0; i--) {
+    let newNode: ListNode | null;
+    if (i === nodeArr.length) {
+      newNode = new ListNode(nodeArr[i]);
+    } else {
+      newNode = new ListNode(nodeArr[i], l3);
+    }
+    l3 = newNode;
+  }
+
+  return l3;
+}
