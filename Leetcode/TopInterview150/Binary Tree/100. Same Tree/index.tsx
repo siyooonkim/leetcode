8efe3e2cf91368eaeedf9eba23463f4ca203db83
siyooonkim <ksyoon0718@gmail.com>
2025-07-20 21:14:50 +0900

    100. Same Tree

--- Leetcode/TopInterview150/Binary Tree/100. Same Tree/index.tsx
+++ Leetcode/TopInterview150/Binary Tree/100. Same Tree/index.tsx
@@ -1,3 +1,4 @@
+// 수정 버전
 /**
  * Definition for a binary tree node.
  * class TreeNode {
@@ -13,19 +14,50 @@
  */
 
 function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
-  const queue = []; // 이거 타입 설정
+  const queue: Array<[TreeNode | null, TreeNode | null]> = [];
   queue.push([p, q]);
 
   while (queue.length) {
     let [pNode, qNode] = queue.shift();
 
-    if ((pNode && !qNode) || (!pNode && qNode)) return false;
+    if (!pNode && !qNode) continue;
 
-    if (pNode && qNode) {
-      if (pNode.val !== qNode.val) return false;
-      queue.push([pNode.left, qNode.left]);
-      queue.push([pNode.right, qNode.right]);
-    }
+    if (!pNode || !qNode || pNode.val !== qNode.val) return false;
+
+    queue.push([pNode.left, qNode.left]);
+    queue.push([pNode.right, qNode.right]);
   }
   return true;
 }
+
+// /**
+//  * Definition for a binary tree node.
+//  * class TreeNode {
+//  *     val: number
+//  *     left: TreeNode | null
+//  *     right: TreeNode | null
+//  *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+//  *         this.val = (val===undefined ? 0 : val)
+//  *         this.left = (left===undefined ? null : left)
+//  *         this.right = (right===undefined ? null : right)
+//  *     }
+//  * }
+//  */
+
+// function isSameTree(p: TreeNode | null, q: TreeNode | null): boolean {
+//   const queue = []; // 이거 타입 설정
+//   queue.push([p, q]);
+
+//   while (queue.length) {
+//     let [pNode, qNode] = queue.shift();
+
+//     if ((pNode && !qNode) || (!pNode && qNode)) return false;
+
+//     if (pNode && qNode) {
+//       if (pNode.val !== qNode.val) return false;
+//       queue.push([pNode.left, qNode.left]);
+//       queue.push([pNode.right, qNode.right]);
+//     }
+//   }
+//   return true;
+// }
