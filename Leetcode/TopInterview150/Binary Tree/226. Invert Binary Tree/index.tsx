00469691a5f379c44dc81ebd5f4c9ccdf7a83514
siyooonkim <ksyoon0718@gmail.com>
2025-07-23 13:57:56 +0900

    226. Invert Binary Tree

--- Leetcode/TopInterview150/Binary Tree/226. Invert Binary Tree/index.tsx
+++ Leetcode/TopInterview150/Binary Tree/226. Invert Binary Tree/index.tsx
@@ -1,3 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function invertTree(root: TreeNode | null): TreeNode | null {
+  let tempNode: TreeNode | null;
+
+  let queue = [];
+  queue.push(root);
+
+  while (queue.length) {
+    let node = queue.shift();
+    if (!node) continue;
+
+    tempNode = node.left;
+    node.left = node.right;
+    node.right = tempNode;
+    queue.push(node.left);
+    queue.push(node.right);
+  }
+
+  return root;
+}
+
+// root의 left와 right 변경
+// root 의 left 와 right 이 이
+
 // 오답, 문제를 완전 잘못 이해
 /**
  * Definition for a binary tree node.
