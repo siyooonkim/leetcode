cd0d785c66dc4b2e75c57561b69fb2a8e10fa227
siyooonkim <ksyoon0718@gmail.com>
2025-07-25 11:34:08 +0900

    101. Symmetric Tree

--- Leetcode/TopInterview150/Binary Tree/101. Symmetric Tree/index.tsx
+++ Leetcode/TopInterview150/Binary Tree/101. Symmetric Tree/index.tsx
@@ -1,3 +1,44 @@
+// 통과는 했음
+// 지나가던 사람이 봐도 코드 가독성 토나올것같음
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSymmetric(root: TreeNode | null): boolean {
+  const queue: (TreeNode | null)[][] = [];
+  queue.push([root.left, root.right]);
+  while (queue.length) {
+    let nodeArr = queue.shift();
+    let valArr = [];
+    let nextArr = [];
+    for (let node of nodeArr) {
+      if (node) {
+        valArr.push(node.val);
+        nextArr.push(node.left);
+        nextArr.push(node.right);
+      } else {
+        valArr.push(null);
+      }
+    }
+    for (let i = 0; i < valArr.length / 2; i++) {
+      if (valArr[i] !== valArr[valArr.length - 1 - i]) return false;
+    }
+
+    if (!nextArr.length) return true;
+    queue.push(nextArr);
+  }
+}
+
 // 완전 오답: mirror의 뜻을 잘못 이해함
 
 /**
